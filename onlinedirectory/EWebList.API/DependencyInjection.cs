32c651ab42a21173069badb87d16cc9c95a18d8b
manektech <devubha@manektech.com>
2021-07-20 12:50:08 +0530

    first commit

---
+++ onlinedirectory/EWebList.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using EWebList.Business.Abstract;
+using EWebList.Business.Concrete;
+using EWebList.Common;
+using EWebList.DataRepository.Abstract;
+using EWebList.DataRepository.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EWebList.API
+{
+    public static class DependencyInjection
+    {
+        public static void AddDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IUserMasterRepository, UserMasterRepository>();
+        }
+    }
+}
---
+++ onlinedirectory/EWebList.API/Startup.cs
@@ -0,0 +1,113 @@
+using EWebList.Business.Abstract;
+using Hangfire;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Text;
+
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+namespace EWebList.API
+{
+    public class Startup
+    {
+        private readonly string CorsPolicy = "CorsPolicy";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDependency();
+
+            services.AddCors();
+            services.AddSwaggerGen(c =>
+            {
+                
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                   
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+            });
+
+           
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicy,
+                    builder =>
+                    {
+                        builder
+                                .AllowAnyOrigin()
+                                .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
+            services.AddMvc(options => { options.Filters.Add(new ApiExceptionFilter()); })
+                .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.AddLog4Net();
+
+            app.UseSwagger();
+
+          
+
+            
+
+            app.UseCors(CorsPolicy);
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+          
+        }
+    }
+}
